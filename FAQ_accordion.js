89b9c7025ae73d538a707e2672c1da4c40064133
Touda-Oulgui <159716903+Touda-Oulgui@users.noreply.github.com>
2024-05-06 18:07:57 +0100

    Add FAQ_accordion.html file

---
+++ FAQ_accordion.js
@@ -0,0 +1,47 @@
+function hidePara1() {
+    document.getElementById('accordion-content1').style.display='none';
+    document.getElementById('plus-icon1').style.display='inline-block';
+    document.getElementById('minus-icon1').style.display='none';
+}
+
+function showPara1() {
+    document.getElementById('accordion-content1').style.display='block';
+    document.getElementById('plus-icon1').style.display='none';
+    document.getElementById('minus-icon1').style.display='inline-block';
+}
+
+function hidePara2() {
+    document.getElementById('accordion-content2').style.display='none';
+    document.getElementById('plus-icon2').style.display='inline-block';
+    document.getElementById('minus-icon2').style.display='none';
+}
+
+function showPara2() {
+    document.getElementById('accordion-content2').style.display='block';
+    document.getElementById('plus-icon2').style.display='none';
+    document.getElementById('minus-icon2').style.display='inline-block';
+}
+
+function hidePara3() {
+    document.getElementById('accordion-content3').style.display='none';
+    document.getElementById('plus-icon3').style.display='inline-block';
+    document.getElementById('minus-icon3').style.display='none';
+}
+
+function showPara3() {
+    document.getElementById('accordion-content3').style.display='block';
+    document.getElementById('plus-icon3').style.display='none';
+    document.getElementById('minus-icon3').style.display='inline-block';
+}
+
+function hidePara4() {
+    document.getElementById('accordion-content4').style.display='none';
+    document.getElementById('plus-icon4').style.display='inline-block';
+    document.getElementById('minus-icon4').style.display='none';
+}
+
+function showPara4() {
+    document.getElementById('accordion-content4').style.display='block';
+    document.getElementById('plus-icon4').style.display='none';
+    document.getElementById('minus-icon4').style.display='inline-block';
+}
